19ab8807a72548da04956e24a58981849326a2e9
illerucis <rob.sicurelli@gmail.com>
2013-12-05 13:39:58 -0500

    brown->frown

--- js/stubbedOutTranslator.js
+++ js/stubbedOutTranslator.js
@@ -1,11 +1,11 @@
 stubbedOutTranslator = {
     "Mae'r": "The",
     "Mae'r llwynog": "The fox",
-    "Mae'r llwynog brown": "The brown fox",
-    "Mae'r llwynog brown cyflym": "The quick brown fox",
-    "Mae'r llwynog brown cyflym neidio": "The quick brown fox jumps",
-    "Mae'r llwynog brown cyflym neidio dros": "The quick brown fox jumps over",
-    "Mae'r llwynog brown cyflym neidio dros y": "The quick brown fox jumps over the",
-    "Mae'r llwynog brown cyflym neidio dros y ci": "The quick brown fox jumps over the dog",
-    "Mae'r llwynog brown cyflym neidio dros y ci ddiog.": "The quick brown fox jumps over the lazy dog."
+    "Mae'r llwynog frown": "The brown fox",
+    "Mae'r llwynog frown cyflym": "The quick brown fox",
+    "Mae'r llwynog frown cyflym neidio": "The quick brown fox jumps",
+    "Mae'r llwynog frown cyflym neidio dros": "The quick brown fox jumps over",
+    "Mae'r llwynog frown cyflym neidio dros y": "The quick brown fox jumps over the",
+    "Mae'r llwynog frown cyflym neidio dros y ci": "The quick brown fox jumps over the dog",
+    "Mae'r llwynog frown cyflym neidio dros y ci ddiog.": "The quick brown fox jumps over the lazy dog."
 };
